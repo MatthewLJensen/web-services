665c5cce43600f1faed6c8b5436a6165e8251c02
Matthew Jensen <matthewljensen@gmail.com>
2022-01-24 09:50:29 -0500

    Through all of chapter 4

--- homework_01/CompanyEmployees/Program.cs
+++ homework_01/CompanyEmployees/Program.cs
@@ -19,6 +19,8 @@ builder.Services.ConfigureServiceManager();
 
 builder.Services.ConfigureSqlContext(builder.Configuration);
 
+builder.Services.AddAutoMapper(typeof(Program));
+
 builder.Services.AddControllers().AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
 
 var app = builder.Build();
--- homework_01/Service/CompanyService.cs
+++ homework_01/Service/CompanyService.cs
@@ -1,23 +1,30 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Service.Contracts;
+using Shared.DataTransferObjects;
+
 namespace Service
 {
     internal sealed class CompanyService : ICompanyService
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
-        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+        private readonly IMapper _mapper;
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
+
         }
-        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        public IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges)
         {
             try
             {
-                var companies =
-                _repository.Company.GetAllCompanies(trackChanges);
-                return companies;
+                var companies = _repository.Company.GetAllCompanies(trackChanges);
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
+                return companiesDto;
             }
             catch (Exception ex)
             {
