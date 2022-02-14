1a320913b9a121b006e9115d899bac477acb0f6d
Matthew Jensen <matthewljensen@gmail.com>
2022-02-14 10:03:24 -0500

    Through most of chapter 12. Getting patchdoc null error.

--- homework_01/CompanyEmployees/MappingProfile.cs
+++ homework_01/CompanyEmployees/MappingProfile.cs
@@ -16,7 +16,7 @@ public class MappingProfile : Profile
 
         CreateMap<EmployeeForCreationDto, Employee>();
 
-        CreateMap<EmployeeForUpdateDto, Employee>();
+        CreateMap<EmployeeForUpdateDto, Employee>().ReverseMap();
 
         CreateMap<CompanyForUpdateDto, Company>();
     }
