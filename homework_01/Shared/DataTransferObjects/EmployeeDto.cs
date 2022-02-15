9d5ff06d81f4696f61a651b06a436bd62d68f01a
Matthew Jensen <matthewljensen@gmail.com>
2022-02-15 13:59:06 -0500

    Through chapter 13

--- homework_01/Shared/DataTransferObjects/EmployeeDto.cs
+++ homework_01/Shared/DataTransferObjects/EmployeeDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Shared.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.DataTransferObjects
 {
     public record EmployeeDto(Guid Id, string Name, int Age, string Position);
-    public record EmployeeForCreationDto(string Name, int Age, string Position);
+    public record EmployeeForCreationDto : EmployeeForManipulationDto;
 
-    public record EmployeeForUpdateDto(string Name, int Age, string Position);
+
+    public record EmployeeForUpdateDto : EmployeeForManipulationDto;
+
 }
