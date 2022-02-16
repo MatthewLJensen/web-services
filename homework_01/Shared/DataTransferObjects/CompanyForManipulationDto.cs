0ec3deb81ae770375df0ed17fe276e81f5cb3ee8
MatthewLJensen <matthewljensen@gmail.com>
2022-02-16 10:19:03 -0500

    Started adding company validator records

---
+++ homework_01/Shared/DataTransferObjects/CompanyForManipulationDto.cs
@@ -0,0 +1,19 @@
+﻿using Shared.DataTransferObjects;
+using System.ComponentModel.DataAnnotations;
+
+public abstract record CompanyForManipulationDto
+{
+
+    public Guid Id { get; init; }
+
+    [Required(ErrorMessage = "Company name is a required field.")]
+    [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
+    public string? Name { get; init; }
+    public string? FullAddress { get; init; }
+
+    public record CompanyForCreationDto(string Name, string Address, string Country,
+    IEnumerable<EmployeeForCreationDto> Employees);
+
+    public record CompanyForUpdateDto(string Name, string Address, string Country,
+    IEnumerable<EmployeeForCreationDto> Employees);
+}
