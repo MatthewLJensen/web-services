cd3e3cde983f30a0df78e91fa808bc06a5000f1d
MatthewLJensen <matthewljensen@gmail.com>
2022-02-21 09:53:11 -0500

    Adding multithreading for the employees.

--- homework_01/Repository/EmployeeRepository.cs
+++ homework_01/Repository/EmployeeRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Contracts;
+using Microsoft.EntityFrameworkCore;
 
 public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
 {
@@ -7,14 +8,15 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
     }
 
-    public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
-    FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
-    .OrderBy(e => e.Name).ToList();
+    public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) =>
+    await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+    .OrderBy(e => e.Name).ToListAsync();
 
-    public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
-        FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id),
+
+    public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
+        await FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id),
         trackChanges)
-        .SingleOrDefault();
+        .SingleOrDefaultAsync();
 
     public void CreateEmployeeForCompany(Guid companyId, Employee employee)
     {
