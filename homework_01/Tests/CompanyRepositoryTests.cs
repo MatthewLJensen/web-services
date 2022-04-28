23167a1aa2d6d0ec84b82b9478d3e84ec55876d5
Matthew Jensen <matthewljensen@gmail.com>
2022-04-28 19:25:08 -0400

    Finished tests

--- homework_01/Tests/CompanyRepositoryTests.cs
+++ homework_01/Tests/CompanyRepositoryTests.cs
@@ -2,6 +2,7 @@ using Contracts;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -13,30 +14,30 @@ public class CompanyRepositoryTests
     public void
     GetAllCompaniesAsync_ReturnsListOfCompanies_WithASingleCompany()
     {
+
+        var cp = new CompanyParameters();
         // Arrange
         var mockRepo = new Mock<ICompanyRepository>();
-        mockRepo.Setup(repo => (repo.GetAllCompaniesAsync(false)))
-        .Returns(Task.FromResult(GetCompanies()));
+        mockRepo.Setup(repo => (repo.GetAllCompaniesAsync(cp, false)))
+            .Returns(Task.FromResult(GetCompanies()));
         // Act
-        var result = mockRepo.Object.GetAllCompaniesAsync(false)
+        var result = mockRepo.Object.GetAllCompaniesAsync(cp, false)
         .GetAwaiter()
-        .GetResult()
-.ToList();
+        .GetResult().ToList();
         // Assert
-        Assert.IsType<List<Company>>(result);
+        Assert.IsAssignableFrom<List<Company>>(result);
         Assert.Single(result);
     }
-    public System.Collections.Generic.IEnumerable<Company> GetCompanies()
+    public PagedList<Company> GetCompanies()
     {
-        return new List<Company>
+        var rval = new PagedList<Company>(new List<Company>(), 1, 1, 1);
+        rval.Add(new Company
         {
-            new Company
-            {
-                Id = Guid.NewGuid(),
-                Name = "Test Company",
-                Country = "United States",
-                Address = "908 Woodrow Way"
-            }
-        };
+            Id = Guid.NewGuid(),
+            Name = "Test Company",
+            Country = "United States",
+            Address = "908 Woodrow Way"
+        });
+        return rval;
     }
 }
